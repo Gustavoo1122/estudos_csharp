eeb76592f626b8c59ff95246cad138d89136ed1a
Gustavo Pereira <gustavope883@gmail.com>
2026-08-18 16:34:47 -0300

    feat: Adiciona projeto smartphone

---
+++ projeto_smartphone/Program.cs
@@ -0,0 +1,14 @@
+﻿using projeto_smartphone.Models;
+
+Nokia android = new Nokia("11 91234-5678", "Lumia", "12285869475838494", 128);
+Iphone ios = new Iphone("11 99876-4321", "17 Pro Max", "8564827492031647", 256);
+
+Console.WriteLine("Smartphone Nokia");
+android.Ligar();
+android.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+ios.ReceberLigacao();
+ios.InstalarAplicativo("Telegram");
